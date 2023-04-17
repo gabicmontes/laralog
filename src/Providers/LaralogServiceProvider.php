a69cf8219125242093a840a3937c46fa6bac5fc7
Gabi Montes <gabicmontes@gmail.com>
2023-04-17 12:11:31 -0300

    create provider

---
+++ src/Providers/LaralogServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Laralog\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaralogServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
